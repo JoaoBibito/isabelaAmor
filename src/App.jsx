e5bde74a7bd933294bab6df259d550847fd186f7
JoaoBibito <joao-vitor1903@live.com>
2023-06-28 16:35:12 -0300

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './assets/components/Header/Header.jsx'
+import Main from './assets/components/.Main/Main'
+
+function App() {
+
+  return (
+    <>
+     <Header/>
+     <Main/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css"
+function Header (){
+  return (
+    <article className="Header">
+      <h1>
+        João Vitor   ❤   Isabela Rodrigues
+      </h1>
+    </article>
+  )
+}
+
+export default Header
